38e5243a5e4926fb97c472d0ed5d6e15e5bf08be
oguncan <elif.barutcu.5353@hotmail.com>
2022-10-16 23:42:20 +0300

    -Adding headers config lines of code

--- src/services/http/scrumboard/endpoints/boardDetail/methods.ts
+++ src/services/http/scrumboard/endpoints/boardDetail/methods.ts
@@ -2,7 +2,9 @@ import service from '../../instance'
 import { BoardsRequestPayload } from './types'
 
 export const updateBoardList = (payload: BoardsRequestPayload) =>
-  service.put('list/'+payload.boardListId, payload)
+  service.put('list/'+payload.boardListId, payload, {
+    headers:{"Authorization": "Bearer " + localStorage.getItem("token")}
+  })
 
   export const createBoardList = (payload: BoardsRequestPayload) =>
   service.post('list', payload, {
@@ -10,10 +12,14 @@ export const updateBoardList = (payload: BoardsRequestPayload) =>
   })
 
   export const deleteBoardList = (payload: BoardsRequestPayload) =>
-  service.delete('list'+payload.boardListId)
+  service.delete('list'+payload.boardListId, {
+    headers:{"Authorization": "Bearer " + localStorage.getItem("token")}
+  })
 
   export const getByIdBoardList = (payload: BoardsRequestPayload) =>
-  service.get('list'+payload.boardListId)
+  service.get('list'+payload.boardListId, {
+    headers:{"Authorization": "Bearer " + localStorage.getItem("token")}
+  })
 
   export const getBoardList = (boardId: number) =>
   service.get('list?'+'boardId='+boardId,{
